edc8e642479e0dc9a47b9aad28d2e845d2172d37
Aníbal Pacheco <apacheco.uy@gmail.com>
2022-11-09 12:33:56 -0300

    templatetag improved

--- utopia_cms_liveblog/templatetags/liveblog_tags.py
+++ utopia_cms_liveblog/templatetags/liveblog_tags.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django import template
 from django.template.loader import render_to_string
 
-from utopia_cms_liveblog.models import LiveBlog
 
 register = template.Library()
 
@@ -11,10 +10,13 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def render_liveblog_notification(context):
     try:
-        liveblog = LiveBlog.objects.get(in_home=True, notification=True)
-        if liveblog.id not in context['request'].session.get('liveblog_notifications_closed', set()):
+        liveblog = context['liveblog']  # if present, is the one with in_home = True (by context processor definition)
+        if (
+            liveblog.notification
+            and liveblog.id not in context['request'].session.get('liveblog_notifications_closed', set())
+        ):
             return render_to_string('utopia_cms_liveblog/liveblog_notification.html', context.flatten())
-    except (LiveBlog.DoesNotExist, LiveBlog.MultipleObjectsReturned):
+    except KeyError:
         pass
     return ""
 
