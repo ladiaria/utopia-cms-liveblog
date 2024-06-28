c3fe6f2edfa862f2ba0ea904d99b94285c247c87
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-06-28 00:03:02 -0300

    admin improvements

--- utopia_cms_liveblog/admin.py
+++ utopia_cms_liveblog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-
-from utopia_cms_liveblog.models import Environment, LiveBlog
+from django.forms import ModelForm, TextInput
+from .models import Environment, LiveBlog
 
 
 @admin.register(Environment)
@@ -10,11 +9,16 @@ class EnvironmentAdmin(admin.ModelAdmin):
     list_editable = ("name", "url")
 
 
+class LiveBlogAdminForm(ModelForm):
+
+    class Meta:
+        widgets = dict((f, TextInput(attrs={'size': 128})) for f in ("title", 'description', "notification_text"))
+
+
 @admin.register(LiveBlog)
 class LiveBlogAdmin(admin.ModelAdmin):
+    form = LiveBlogAdminForm
     list_display = ("day", "environment", "title", "status", "in_home", "notification")
     raw_id_fields = ("image", )
     list_filter = ("status", )
     date_hierarchy = "day"
-
-
