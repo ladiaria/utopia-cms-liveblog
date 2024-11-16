859e420f9753049111462c3dced0880bfd367b95
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-11-15 23:24:29 -0300

    better_fix

--- utopia_cms_liveblog/static/js/liveblog_notification.js
+++ utopia_cms_liveblog/static/js/liveblog_notification.js
@@ -1,13 +1,19 @@
 function load_liveblog_notification(base_path, publication_slug){
+  let trigger_event = false;
   if (base_path && publication_slug) {
     $("#liveblog-notifications").load("/" + base_path + "/notification/" + publication_slug + "/", function(response){
-      if(response){
+      if (response) {
         $(".btn-close", this).click(function(){
           notification_close(this);
         });
-      }else{
-        $(window).trigger("liveblog_notification_empty");
+      } else {
+        trigger_event = true;
       }
     });
+  } else {
+    trigger_event = true;
+  }
+  if (trigger_event) {
+    $(window).trigger("liveblog_notification_empty");
   }
 }
