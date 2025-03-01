8c7e1ce61586ee63eb4f79166e81748c24ca89fa
Agus Vargas <agustinvargas93@gmail.com>
2025-03-01 13:15:54 -0300

    Add is_frontpage parameter to liveblog_embed_code for conditional iframe URL

--- utopia_cms_liveblog/templatetags/liveblog_tags.py
+++ utopia_cms_liveblog/templatetags/liveblog_tags.py
@@ -6,8 +6,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def liveblog_embed_code(liveblog_obj, disable_iframe_resizer=False):
+def liveblog_embed_code(liveblog_obj, disable_iframe_resizer=False, is_frontpage=False):
     return render_to_string(
         'utopia_cms_liveblog/liveblog_embed_code.html',
-        {"object": liveblog_obj, "disable_iframe_resizer": disable_iframe_resizer},
+        {"object": liveblog_obj, "disable_iframe_resizer": disable_iframe_resizer, "is_frontpage": is_frontpage},
     )
