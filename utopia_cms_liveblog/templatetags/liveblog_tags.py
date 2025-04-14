675822d05992617e95360033c1af7ac3e3d7b996
Aníbal Pacheco <apacheco.uy@gmail.com>
2025-04-14 06:06:31 -0300

    syntax code style fixes only

--- utopia_cms_liveblog/templatetags/liveblog_tags.py
+++ utopia_cms_liveblog/templatetags/liveblog_tags.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django import template
 from django.template.loader import render_to_string
-from django.conf import settings
+
 
 register = template.Library()
 
 
 @register.simple_tag()
-def liveblog_embed_code(liveblog_obj, disable_iframe_resizer=False, is_frontpage=False, allowAttrs=False, is_post_anchor=False):
+def liveblog_embed_code(
+    liveblog_obj, disable_iframe_resizer=False, is_frontpage=False, allowAttrs=False, is_post_anchor=False
+):
     query_params = []
     if is_frontpage:
         query_params.append('is_frontpage=1')
@@ -17,5 +20,11 @@ def liveblog_embed_code(liveblog_obj, disable_iframe_resizer=False, is_frontpage
     query_params_str = '&'.join(query_params) if query_params else ''
     return render_to_string(
         'utopia_cms_liveblog/liveblog_embed_code.html',
-        {"object": liveblog_obj, "disable_iframe_resizer": disable_iframe_resizer, "is_frontpage": is_frontpage, "allowAttrs": allowAttrs, "query_params_str": query_params_str},
+        {
+            "object": liveblog_obj,
+            "disable_iframe_resizer": disable_iframe_resizer,
+            "is_frontpage": is_frontpage,
+            "allowAttrs": allowAttrs,
+            "query_params_str": query_params_str,
+        },
     )
